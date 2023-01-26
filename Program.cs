296ffb9f400e2f966b1df56ff13528cbcc288d37
Irina632015 <irina.632015@yandex.ru>
2023-01-26 20:55:39 +0400

    Семинар 1  Задача 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int Num1 = Convert.ToInt32(Console.ReadLine());
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1 > Num2)
+
+{
+    Console.WriteLine(Num1 + " > " + Num2);
+}
+else if (Num2 > Num1)
+{
+    Console.WriteLine(Num2 + " > " + Num1);
+}
+else
+{
+    Console.WriteLine(Num2 + " = " + Num1);
+}
